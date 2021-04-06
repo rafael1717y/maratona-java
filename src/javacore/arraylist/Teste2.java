8fa2285c25ffc5b94d59b65020dc9c99e5fd6c46
rafael1717y <rafaelufjf@gmail.com>
2021-04-06 07:53:54 -0300

    ArrayList

--- src/javacore/arraylist/Teste2.java
+++ src/javacore/arraylist/Teste2.java
@@ -8,12 +8,14 @@ public class Teste2 {
 
     public static void main(String[] args) {
 
-        ArrayList lista = new ArrayList();
+        //Generics. Uma classe genérica mas parametrizada na criação.
+        ArrayList<Conta> lista = new ArrayList<Conta>();
+        ArrayList<String> nomes = new ArrayList<String>();
 
         Conta cc = new ContaCorrente(22, 11);
         lista.add(cc);
 
-        Conta cc2 = new ContaCorrente(22, 11);
+         Conta cc2 = new ContaCorrente(22, 11);
         lista.add(cc2);
 
         Conta cc3 = new ContaCorrente(22, 13);
@@ -23,7 +25,7 @@ public class Teste2 {
         lista.add(cc4);
 
         System.out.println(lista.size());
-        Conta ref = (Conta) lista.get(0);
+        Conta ref = lista.get(0);
         System.out.println(ref.getNumero());
 
         lista.remove(0);
@@ -35,8 +37,8 @@ public class Teste2 {
         }
 
         //enhanced for
-        for(Object oRef : lista) {
-            System.out.println(oRef);
+        for(Conta conta : lista) {
+            System.out.println(conta);
         }
 
 
